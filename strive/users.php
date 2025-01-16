6856742842342ff72a1d5f44998fb958d5b5b67a
yuri <85718637+yurizin035@users.noreply.github.com>
2025-01-16 00:15:25 -0300

    Update users.php

--- strive/users.php
+++ strive/users.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *"); // Permite qualquer origem
+header("Access-Control-Allow-Origin: https://strivepay.web.app"); // Permite apenas origem do site strivepay.web.app
 header("Access-Control-Allow-Methods: GET, OPTIONS"); // Permite GET e OPTIONS
 header("Access-Control-Allow-Headers: Content-Type"); // Permite o cabeçalho Content-Type
 
